36fce0a3a138b421da9c2cc6a1db7d45524b1141
Alex White <awhit012@gmail.com>
2015-09-08 12:33:29 -0700

    work in progress, cross-origin-issue

--- app.rb
+++ app.rb
@@ -8,17 +8,31 @@ get '/' do
 end
 
 get '/favorites' do
+  response['Access-Control-Allow-Origin'] = '*'
   response.header['Content-Type'] = 'application/json'
   File.read('data.json')
 end
 
 post '/favorite' do
   # response['Access-Control-Allow-Origin'] = 'http://domain.com'
+
+  request['Access-Control-Request-Method'] = '*'
+  request['Access-Control-Allow-Origin'] = '*'
   response['Access-Control-Allow-Origin'] = '*'
+  request['Access-Control-Allow-Origin'] = 'file:///Users/awhit012/Desktop/instructor-code-challenge-master/js-frontend/index.html'
+  response['Access-Control-Allow-Origin'] = 'file:///Users/awhit012/Desktop/instructor-code-challenge-master/js-frontend/index.html'
+
+  response['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+  response['Access-Control-Request-Method'] = '*'
+  response['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+
+
+
   content_type :json
   # p JSON.parse(response.body.read)
   p response
   p params
+  p params.to_json
 
   # file = JSON.parse(File.read('data.json'))
   # return 'Invalid Request' unless params[:name] && params[:oid]
