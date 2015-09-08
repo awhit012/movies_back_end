733b395300bc6ae75d3212a902ffbd4c1f498790
Alex White <awhit012@gmail.com>
2015-09-07 22:44:31 -0700

    working on json post from front end

--- app.rb
+++ app.rb
@@ -1,20 +1,29 @@
 require 'sinatra'
-
+require 'json'
+require 'httparty'
 
 get '/' do
   File.read(File.join('./views', 'index.html'))
+  # HTTParty.get('http://www.omdbapi.com/?s=hello&y=&plot=short&r=json')
 end
 
-get 'favorites' do
+get '/favorites' do
   response.header['Content-Type'] = 'application/json'
   File.read('data.json')
 end
 
-get '/favorites' do
-  file = JSON.parse(File.read('data.json'))
-  return 'Invalid Request' unless params[:name] && params[:oid]
-  movie = { name: params[:name], oid: params[:oid] }
-  file << movie
-  File.write('data.json',JSON.pretty_generate(file))
-  movie.to_json
+post '/favorite' do
+  # response['Access-Control-Allow-Origin'] = 'http://domain.com'
+  response['Access-Control-Allow-Origin'] = '*'
+  content_type :json
+  # p JSON.parse(response.body.read)
+  p response
+  p params
+
+  # file = JSON.parse(File.read('data.json'))
+  # return 'Invalid Request' unless params[:name] && params[:oid]
+  # movie = { name: params[:name], oid: params[:oid] }
+  # file << movie
+  # File.write('data.json',JSON.pretty_generate(file))
+  # movie.to_json
 end
